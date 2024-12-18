a36a2359190ab2878c2b1dda8fa349cda40cba3e
Raf Ponsaerts <rafponsaerts@Rafs-MacBook-Pro.local>
2024-12-18 14:52:10 +0100

    Small restructuring of the WineShop file

--- src/main/kotlin/be/athumi/WineShop.kt
+++ src/main/kotlin/be/athumi/WineShop.kt
@@ -14,7 +14,6 @@ class WineShop(var items: List<Wine>) {
         val EVENT_PRICE_INCREASE_SECOND_BREAKPOINT = 4
     }
 
-
     fun next() {
         // Wine Shop logic
         for (wine in items) {
@@ -31,42 +30,6 @@ class WineShop(var items: List<Wine>) {
         }
     }
 
-    private fun handleEventWine(wine: Wine) {
-        wine.expiresInYears -= 1
-        if (wine.expiresInYears < EVENT_EXPIRATION_SECOND_BREAKPOINT) {
-            increasePrice(wine, EVENT_PRICE_INCREASE_SECOND_BREAKPOINT)
-        } else if (wine.expiresInYears < EVENT_EXPIRATION_FIRST_BREAKPOINT) {
-            increasePrice(wine, EVENT_PRICE_INCREASE_FIRST_BREAKPOINT)
-        } else {
-            increasePrice(wine, DEFAULT_PRICE_INCREASE)
-        }
-
-        if (wine.expiresInYears < 0) setToMinimumPrice(wine)
-    }
-
-    private fun handleDefaultWine(wine: Wine) {
-        wine.expiresInYears -= 1
-        if (wine.expiresInYears < 0) {
-            decreasePrice(wine, 2 * DEFAULT_PRICE_DECREASE)
-        } else {
-            decreasePrice(wine, DEFAULT_PRICE_DECREASE)
-        }
-    }
-
-    private fun handleAgingWine(wine: Wine) {
-        wine.expiresInYears -= 1
-        if (wine.expiresInYears < 0) {
-            increasePrice(wine, 2 * DEFAULT_PRICE_DECREASE)
-        } else {
-            increasePrice(wine, DEFAULT_PRICE_DECREASE)
-        }
-    }
-
-    private fun handleSpecialWine(wine: Wine) {
-        // Nothing needs to happen with special wines yet
-        return
-    }
-
     private fun getWineType(wine: Wine): WineTypeEnum {
         if (wine.name == "Wine brewed by Alexander the Great") {
             return WineTypeEnum.SPECIAL
@@ -81,8 +44,40 @@ class WineShop(var items: List<Wine>) {
         }
     }
 
-    private fun setToMinimumPrice(wine: Wine) {
-        wine.price = MIN_PRICE
+    private fun handleEventWine(wine: Wine) {
+        decrementExpiresInYears(wine)
+        if (wine.expiresInYears < EVENT_EXPIRATION_SECOND_BREAKPOINT) {
+            increasePrice(wine, EVENT_PRICE_INCREASE_SECOND_BREAKPOINT)
+        } else if (wine.expiresInYears < EVENT_EXPIRATION_FIRST_BREAKPOINT) {
+            increasePrice(wine, EVENT_PRICE_INCREASE_FIRST_BREAKPOINT)
+        } else {
+            increasePrice(wine, DEFAULT_PRICE_INCREASE)
+        }
+
+        if (wine.expiresInYears < 0) setToMinimumPrice(wine)
+    }
+
+    private fun handleDefaultWine(wine: Wine) {
+        decrementExpiresInYears(wine)
+        if (wine.expiresInYears < 0) {
+            decreasePrice(wine, 2 * DEFAULT_PRICE_DECREASE)
+        } else {
+            decreasePrice(wine, DEFAULT_PRICE_DECREASE)
+        }
+    }
+
+    private fun handleAgingWine(wine: Wine) {
+        decrementExpiresInYears(wine)
+        if (wine.expiresInYears < 0) {
+            increasePrice(wine, 2 * DEFAULT_PRICE_DECREASE)
+        } else {
+            increasePrice(wine, DEFAULT_PRICE_DECREASE)
+        }
+    }
+
+    private fun handleSpecialWine(wine: Wine) {
+        // Nothing needs to happen with special wines yet
+        return
     }
 
     private fun increasePrice(wine: Wine, amount: Int) {
@@ -96,4 +91,13 @@ class WineShop(var items: List<Wine>) {
             wine.price = wine.price - amount
         }
     }
+
+    private fun setToMinimumPrice(wine: Wine) {
+        wine.price = MIN_PRICE
+    }
+
+    private fun decrementExpiresInYears(wine: Wine) {
+        wine.expiresInYears -= 1
+    }
+
 }
